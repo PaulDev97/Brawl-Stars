82d049d12d9740a00a4a440f06f84f626fa33c7a
PaulDev97 <paul.devc@gmail.com>
2023-09-28 19:52:37 -0300

    cards

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main>
+    <main className=' h-screen flex flex-col  items-center'>
       <h1 className=''>Paginas Principal</h1>
+
+      <section className='w-[80%] flex flex-wrap gap-2 justify-center items-center'>
+        <div className='w-[180px] h-[120px]'>
+          <img className='w-full h-full rounded-lg' src="./assets/home/brawl_halloween.jpg" alt="halloween" />
+        </div>
+        <div className='w-[180px] h-[120px]'>
+          <img className='w-full h-full rounded-lg' src="./assets/home/brawl-stars-championship-2023.jpg" alt="championship" />
+        </div>
+        <div className='w-[180px] h-[120px]'>
+          <img className='w-full h-full rounded-lg' src="./assets/home/brawl_pass.jpg" alt="brawl pass" />
+        </div>
+
+      </section>
     </main>
   )
 }
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -6,11 +6,11 @@ import Menu from '../Menu/Menu'
 
 const Layout = ({ children }) => {
   return (
-    <div className='w-full h-full   flex '>
+    <div className='w-full h-full flex  '>
 
       <Menu />
 
-      <div className='w-[80%] absolute right-0  bg-[#cee3f2]  ' >
+      <div className='w-[79%] absolute right-0  bg-[#ebf2f6]  ' >
         {children}
       </div>
 
